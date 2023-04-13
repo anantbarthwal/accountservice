5b5f2b7adc8c39809549c4f353ba3c012d10d396
Anant Barthwal <anant.barthwal@citiustech.com>
2023-04-13 23:55:37 +0530

    account-service: added rate circuitbreaker

--- src/main/java/com/bookings/accountservice/controllers/AccountController.java
+++ src/main/java/com/bookings/accountservice/controllers/AccountController.java
@@ -3,6 +3,7 @@ package com.bookings.accountservice.controllers;
 import com.bookings.accountservice.handlers.AccountHandler;
 import com.bookings.accountservice.models.Account;
 import com.bookings.accountservice.models.AccountCreateRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ public class AccountController {
     @Autowired
     private AccountHandler accountHandler;
     @PostMapping("/create")
+    @CircuitBreaker(name = "rateCircuitBreaker", fallbackMethod = "rateFallBack")
     public Account createAccount(@RequestBody AccountCreateRequest request) {
        return accountHandler.createAccount(request);
     }
@@ -21,5 +23,9 @@ public class AccountController {
     @GetMapping("/id/{id}")
     public Account getById(@PathVariable String id) {
         return accountHandler.getAccountById(id);
+    }
+
+    public Account rateFallBack(AccountCreateRequest request, Exception ex) {
+        return new Account();
     }
 }
